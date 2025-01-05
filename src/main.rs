7263c1f2fda0bd4de1199cd92cb79440c04c97d3
Dennis Dang <dangggdennis@gmail.com>
2025-01-04 21:30:03 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use anyhow::{Context, Result};
 use std::collections::HashMap;
 use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
-use std::str::Utf8Error;
 
 fn main() -> Result<(), ()> {
     let http_server = HttpServer::new();
@@ -19,24 +18,32 @@ impl HttpServer {
     fn listen(&self) -> Result<(), ()> {
         let listener = TcpListener::bind("127.0.0.1:4221")
             .context("Failed to bind to address")
-            .map_err(|_err| ())?;
+            .map_err(|err| {
+                eprintln!("{:?}", err);
+                ()
+            })?;
 
         for stream in listener.incoming() {
-            let mut tcp_stream = stream
-                .context("Failed to accept connection")
-                .map_err(|_err| ())?;
-
-            let _ = self
-                .handle_connection(&mut tcp_stream)
-                .context("Failed to handle connection");
+            match stream {
+                Ok(mut tcp_stream) => {
+                    if let Err(err) = self
+                        .handle_connection(&mut tcp_stream)
+                        .context("Failed to handle connection")
+                    {
+                        eprintln!("{:?}", err);
+                    }
+                }
+                Err(err) => {
+                    eprintln!("Failed to accept connection: {:?}", err);
+                }
+            }
         }
 
         Ok(())
     }
 
-    fn parse_request(input: &[u8]) -> Result<Request, ParseError> {
-        let req_str = std::str::from_utf8(input)?;
-        let mut lines = req_str.lines();
+    fn parse_request(input: &str) -> Result<Request, ParseError> {
+        let mut lines = input.lines().peekable();
         let req_line = lines.next().ok_or(ParseError::InvalidRequest)?;
         let mut parts = req_line.split_whitespace();
 
@@ -48,10 +55,27 @@ impl HttpServer {
         let version = parts.next().ok_or(ParseError::InvalidRequest)?;
         let version = Self::parse_version(version)?;
 
+        let mut headers = HashMap::new();
+        let mut body = None;
+
+        while let Some(line) = lines.next() {
+            if line.is_empty() {
+                // Empty line indicates the end of headers and start of body
+                body = Some(lines.collect::<Vec<&str>>().join("\n"));
+                break;
+            }
+
+            if let Some((key, value)) = line.split_once(": ") {
+                headers.insert(key.to_lowercase().to_string(), value.to_string());
+            }
+        }
+
         Ok(Request {
             method,
             path,
             version,
+            headers,
+            body,
         })
     }
 
@@ -77,51 +101,67 @@ impl HttpServer {
 
     fn handle_connection(&self, tcp_stream: &mut std::net::TcpStream) -> Result<()> {
         let mut reader = BufReader::new(tcp_stream.by_ref());
-        let mut buffer = Vec::new();
+        let mut request_lines = Vec::new();
 
-        // Read headers
         loop {
             let mut line = String::new();
             reader.read_line(&mut line).context("Failed to read line")?;
             if line == "\r\n" {
                 break;
             }
-            buffer.extend_from_slice(line.as_bytes());
+            request_lines.extend_from_slice(line.as_bytes());
         }
 
-        let response: String = match HttpServer::parse_request(&buffer) {
+        let request_lines = std::str::from_utf8(&request_lines)?;
+
+        let response: String = match HttpServer::parse_request(request_lines) {
             Ok(request) => {
                 let path_vec = request.path.split('/').collect::<Vec<&str>>();
                 let path_parts = path_vec.as_slice();
                 match path_parts {
-                    ["", ""] => {
-                        println!("base root");
-                        Response {
-                            body: "".to_string(),
-                            headers: HashMap::new(),
-                            status_code: 200,
-                            version: request.version,
-                        }
-                        .to_http_string()
+                    ["", ""] => Response {
+                        body: None,
+                        headers: HashMap::new(),
+                        status_code: 200,
+                        version: request.version,
                     }
-                    ["", "echo", echo_str] => {
-                        let body = (*echo_str).to_string();
-
-                        let mut headers = HashMap::new();
-                        headers.insert("Content-Type".into(), "text/plain".into());
-                        headers.insert("Content-Length".into(), body.len().to_string());
+                    .to_http_string(),
+                    ["", "user-agent"] => {
+                        let ua = request.headers.get("user-agent").cloned();
+                        let mut resp_headers = HashMap::new();
+                        resp_headers.insert("Content-Type".into(), "text/plain".into());
+                        resp_headers.insert(
+                            "Content-Length".into(),
+                            ua.as_ref()
+                                .map_or("0".to_string(), |ua_str| ua_str.len().to_string()),
+                        );
 
                         let res = Response {
                             status_code: 200,
                             version: request.version,
-                            headers,
-                            body,
+                            headers: resp_headers,
+                            body: ua,
+                        };
+
+                        res.to_http_string()
+                    }
+                    ["", "echo", echo_str] => {
+                        let body = (*echo_str).to_string();
+                        let mut resp_headers = HashMap::new();
+                        resp_headers.insert("Content-Type".into(), "text/plain".into());
+                        resp_headers.insert("Content-Length".into(), body.len().to_string());
+
+                        let res = Response {
+                            status_code: 200,
+                            version: request.version,
+                            headers: resp_headers,
+                            body: Some(body),
                         };
 
                         res.to_http_string()
                     }
                     _ => Response {
-                        body: "".into(),
+                        body: None,
                         headers: HashMap::new(),
                         status_code: 404,
                         version: request.version,
@@ -132,7 +172,7 @@ impl HttpServer {
             Err(e) => {
                 println!("failed to parse request: {:?}", e);
                 Response {
-                    body: "".into(),
+                    body: None,
                     headers: HashMap::new(),
                     status_code: 404,
                     version: Version::Http1_1,
@@ -154,6 +194,8 @@ struct Request<'a> {
     method: Method,
     path: &'a str,
     version: Version,
+    headers: HashMap<String, String>,
+    body: Option<String>,
 }
 
 #[derive(Debug)]
@@ -161,7 +203,7 @@ struct Response {
     status_code: u32,
     version: Version,
     headers: HashMap<String, String>,
-    body: String,
+    body: Option<String>,
 }
 
 impl Response {
@@ -179,7 +221,12 @@ impl Response {
             .collect::<Vec<String>>()
             .join("\r\n");
 
-        format!("{}\r\n{}\r\n\r\n{}", status_line, headers, self.body)
+        format!(
+            "{}\r\n{}\r\n\r\n{}",
+            status_line,
+            headers,
+            self.body.clone().unwrap_or_else(|| "".to_string())
+        )
     }
 
     fn reason_phrase(&self) -> &str {
@@ -197,13 +244,6 @@ enum ParseError {
     InvalidRequest,
     InvalidMethod,
     InvalidVersion,
-    Utf8Error(Utf8Error),
-}
-
-impl From<Utf8Error> for ParseError {
-    fn from(err: Utf8Error) -> Self {
-        ParseError::Utf8Error(err)
-    }
 }
 
 #[derive(Debug)]
