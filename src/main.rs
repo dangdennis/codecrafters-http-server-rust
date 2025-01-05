978a2e46316566b8a140696f89be8208bf3dbdce
Dennis Dang <dangggdennis@gmail.com>
2025-01-04 23:49:06 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use anyhow::{Context, Result};
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
 use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
 use std::sync::Arc;
 use std::thread;
 
 fn main() -> Result<(), ()> {
-    let http_server = CodeCraftsHttpServer::new();
-    http_server.listen()
+    let args: Vec<String> = std::env::args().collect();
+
+    let directory = args
+        .iter()
+        .position(|arg| arg == "--directory")
+        .and_then(|pos| args.get(pos + 1));
+
+    let http_server = CodeCraftsHttpServer::new(directory);
+
+    http_server.start()
 }
 
 struct CodeCraftsHttpServer {
@@ -15,13 +25,15 @@ struct CodeCraftsHttpServer {
 }
 
 impl CodeCraftsHttpServer {
-    fn new() -> Self {
+    fn new(file_dir: Option<&String>) -> Self {
         Self {
-            server: Arc::new(HttpServer {}),
+            server: Arc::new(HttpServer {
+                file_dir: file_dir.cloned(),
+            }),
         }
     }
 
-    fn listen(self) -> Result<(), ()> {
+    fn start(self) -> Result<(), ()> {
         let listener = TcpListener::bind("127.0.0.1:4221")
             .context("Failed to bind to address")
             .map_err(|err| {
@@ -52,66 +64,11 @@ impl CodeCraftsHttpServer {
     }
 }
 
-struct HttpServer {}
+struct HttpServer {
+    file_dir: Option<String>,
+}
 
 impl HttpServer {
-    fn parse_request(input: &str) -> Result<Request, ParseError> {
-        let mut lines = input.lines().peekable();
-        let req_line = lines.next().ok_or(ParseError::InvalidRequest)?;
-        let mut parts = req_line.split_whitespace();
-
-        let method = parts.next().ok_or(ParseError::InvalidRequest)?;
-        let method = Self::parse_method(method)?;
-
-        let path = parts.next().ok_or(ParseError::InvalidRequest)?;
-
-        let version = parts.next().ok_or(ParseError::InvalidRequest)?;
-        let version = Self::parse_version(version)?;
-
-        let mut headers = HashMap::new();
-        let mut body = None;
-
-        while let Some(line) = lines.next() {
-            if line.is_empty() {
-                // Empty line indicates the end of headers and start of body
-                body = Some(lines.collect::<Vec<&str>>().join("\n"));
-                break;
-            }
-
-            if let Some((key, value)) = line.split_once(": ") {
-                headers.insert(key.to_lowercase().to_string(), value.to_string());
-            }
-        }
-
-        Ok(Request {
-            method,
-            path,
-            version,
-            headers,
-            body,
-        })
-    }
-
-    fn parse_method(method: &str) -> Result<Method, ParseError> {
-        match method {
-            "GET" => Ok(Method::Get),
-            "POST" => Ok(Method::Post),
-            "PUT" => Ok(Method::Put),
-            "DELETE" => Ok(Method::Delete),
-            "PATCH" => Ok(Method::Patch),
-            _ => Err(ParseError::InvalidMethod),
-        }
-    }
-
-    fn parse_version(version: &str) -> Result<Version, ParseError> {
-        match version {
-            "HTTP/1.0" => Ok(Version::Http1_0),
-            "HTTP/1.1" => Ok(Version::Http1_1),
-            "HTTP/2.0" => Ok(Version::Http2_0),
-            _ => Err(ParseError::InvalidVersion),
-        }
-    }
-
     fn handle_connection(&self, mut tcp_stream: std::net::TcpStream) -> Result<()> {
         let mut reader = BufReader::new(&tcp_stream);
         let mut request_lines = Vec::new();
@@ -173,6 +130,56 @@ impl HttpServer {
 
                         res.to_http_string()
                     }
+                    ["", "files", filename] => {
+                        if let Some(file_dir) = &self.file_dir {
+                            let file_path = format!("{}/{}", file_dir, filename);
+
+                            let mut resp_headers = HashMap::<String, String>::new();
+                            resp_headers
+                                .insert("Content-Type".into(), "application/octet-stream".into());
+
+                            let file_content = match File::open(&file_path) {
+                                Ok(mut file) => {
+                                    let mut content = String::new();
+                                    file.read_to_string(&mut content)
+                                        .context("Failed to read file")?;
+                                    Some(content)
+                                }
+                                Err(_) => None,
+                            };
+
+                            if let Some(body) = file_content {
+                                resp_headers
+                                    .insert("Content-Length".into(), body.len().to_string());
+                                let res = Response {
+                                    status_code: 200,
+                                    version: request.version,
+                                    headers: resp_headers,
+                                    body: Some(body),
+                                };
+
+                                res.to_http_string()
+                            } else {
+                                let res = Response {
+                                    status_code: 404,
+                                    version: request.version,
+                                    headers: HashMap::new(),
+                                    body: None,
+                                };
+
+                                res.to_http_string()
+                            }
+                        } else {
+                            let res = Response {
+                                status_code: 404,
+                                version: request.version,
+                                headers: HashMap::new(),
+                                body: None,
+                            };
+
+                            res.to_http_string()
+                        }
+                    }
                     _ => Response {
                         body: None,
                         headers: HashMap::new(),
@@ -199,6 +206,63 @@ impl HttpServer {
             .context("Failed to write response")?;
 
         Ok(())
+    }
+
+    fn parse_request(input: &str) -> Result<Request, ParseError> {
+        let mut lines = input.lines().peekable();
+        let req_line = lines.next().ok_or(ParseError::InvalidRequest)?;
+        let mut parts = req_line.split_whitespace();
+
+        let method = parts.next().ok_or(ParseError::InvalidRequest)?;
+        let method = Self::parse_method(method)?;
+
+        let path = parts.next().ok_or(ParseError::InvalidRequest)?;
+
+        let version = parts.next().ok_or(ParseError::InvalidRequest)?;
+        let version = Self::parse_version(version)?;
+
+        let mut headers = HashMap::new();
+        let mut body = None;
+
+        while let Some(line) = lines.next() {
+            if line.is_empty() {
+                // Empty line indicates the end of headers and start of body
+                body = Some(lines.collect::<Vec<&str>>().join("\n"));
+                break;
+            }
+
+            if let Some((key, value)) = line.split_once(": ") {
+                headers.insert(key.to_lowercase().to_string(), value.to_string());
+            }
+        }
+
+        Ok(Request {
+            method,
+            path,
+            version,
+            headers,
+            body,
+        })
+    }
+
+    fn parse_method(method: &str) -> Result<Method, ParseError> {
+        match method {
+            "GET" => Ok(Method::Get),
+            "POST" => Ok(Method::Post),
+            "PUT" => Ok(Method::Put),
+            "DELETE" => Ok(Method::Delete),
+            "PATCH" => Ok(Method::Patch),
+            _ => Err(ParseError::InvalidMethod),
+        }
+    }
+
+    fn parse_version(version: &str) -> Result<Version, ParseError> {
+        match version {
+            "HTTP/1.0" => Ok(Version::Http1_0),
+            "HTTP/1.1" => Ok(Version::Http1_1),
+            "HTTP/2.0" => Ok(Version::Http2_0),
+            _ => Err(ParseError::InvalidVersion),
+        }
     }
 }
 
